f8d521528fcfc5458373327fc4967b0a6845e560
firas-debich <debichf02@gmail.com>
2022-01-19 15:34:21 +0100

    card image done

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import {Chip,CardActions,CardContent,CardMedia,Card,Button,Typography,Box}  from "@material-ui/core"
+import  LocationOnIcon from "@material-ui/icons/LocationOn"
+import  PhoneIcon from "@material-ui/icons/Phone"
+import Rating from "@material-ui/lab/Rating"
+import useStyles from './styles.js';
 function PlaceDetails({place}) {
+    const classes = useStyles();
     return (
-        <h1>
-                {place.name}
-        </h1>
+      <Card elevation={6}>
+          <CardMedia
+            style={{height:350}}
+            image={place.photo ? place.photo.images.large.url : 'https://media-cdn.tripadvisor.com/media/photo-s/1a/18/3a/cb/restaurant-le-47.jpg'}
+            title={place.name}
+          
+          
+          />
+
+
+      </Card>
     )
 }
 

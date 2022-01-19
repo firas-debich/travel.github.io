a6899d1dcb6502c2a8dd36ef568b7b80a4e337d2
firas-debich <debichf02@gmail.com>
2022-01-19 17:25:50 +0100

    filtring the data based on rating

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import React ,{useEffect,useState}from "react";
+import React, { useEffect, useState } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
-import {getPlacesData} from './api/index';
-
+import { getPlacesData } from "./api/index";
 
 function App() {
-  const [data , setData]=useState([])
-  const [coordinates , setCoordinates]=useState({})
-  const [bounds , setBounds]=useState(null)
-  const [childClicked, setChildClicked] = useState(null)
+  const [data, setData] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-   const [type, setType] = useState("restaurants");
+  const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
-  useEffect(()=>{
-      navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude}})=>{
-          setCoordinates({lat:latitude,lng:longitude})
-      })
-  },[])
+  const [filtredPlaces, setFilteredPlaces] = useState(null);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+  useEffect(() => {
+    const filtered = data.filter((place) => Number(place.rating) > rating);
 
-  useEffect(()=>{
-    setIsLoading(true)
-     getPlacesData( type ,bounds?.sw,bounds?.ne)
-        .then((data)=>{
-          setData(data)
-          setIsLoading(false)
-        })  
-  },[bounds,coordinates,type])
+    setFilteredPlaces(filtered);
+  }, [rating]);
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(type, bounds?.sw, bounds?.ne).then((data) => {
+
+      setData(data);
+      setFilteredPlaces(null)
+      setIsLoading(false);
+    });
+  }, [bounds, coordinates, type]);
 
   return (
     <>
@@ -35,13 +42,14 @@ function App() {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List places={data}
-          childClicked={childClicked}
-          isLoading={isLoading}
-          type={type}
-          setType={setType}
-          rating={rating}
-          setRating={setRating}
+          <List
+            places={filtredPlaces ? filtredPlaces : data}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
           />
         </Grid>
         <Grid item xs={12} md={8}>
@@ -49,12 +57,11 @@ function App() {
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
-            places={data}
+            places={filtredPlaces ? filtredPlaces : data}
             setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
-   
     </>
   );
 }

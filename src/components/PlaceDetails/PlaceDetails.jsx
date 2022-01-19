09028909002ffbfd857ca77386bb3f7b41ca9974
firas-debich <debichf02@gmail.com>
2022-01-19 15:51:28 +0100

    adding cuisine information

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -37,7 +37,9 @@ function PlaceDetails({place}) {
             <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
           </Box>
         ))}
-
+           {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className={classes.chip} />
+        ))}
             </CardContent>
       </Card>
     )
